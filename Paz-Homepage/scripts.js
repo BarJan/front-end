131660c6fef991413d93ac258480bb38f9820834
barjan <bardugo1@protonmail.com>
2020-11-25 00:00:09 +0200

    created and designed contact page

---
+++ Paz-Homepage/scripts.js
@@ -0,0 +1,13 @@
+function contactPage(){
+    document.getElementById('main').className = 'contact';
+    document.getElementById('header').className = 'contact';
+    document.getElementById('footer').className = 'contact';
+    document.getElementById('formain').className = 'contact'
+}
+
+function homePage(){
+    document.getElementById('main').className = '';
+    document.getElementById('header').className = '';
+    document.getElementById('footer').className = '';
+    document.getElementById('formain').className = ''
+}
